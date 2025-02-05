e7885313e9b1b5015f582da36b9eea00a67c13d2
Karushi536 <karushiabigael@gmail.com>
2025-02-05 23:03:26 +0300

    spoon

---
+++ spoon/api/myapp/views.py
@@ -0,0 +1,20 @@
+from django.utils import timezone
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import MyModelSerializer
+from .models import MyModel
+
+@api_view(['GET'])
+def my_model_view(request):
+    try:
+        instance = MyModel.objects.latest('current_datetime')  # Use the correct field name
+    except MyModel.DoesNotExist:
+        return Response({"error": "No data found"}, status=404)
+    
+    serializer = MyModelSerializer(instance)
+    data = serializer.data
+
+    current_time = timezone.now().replace(microsecond=0).strftime("%Y-%m-%dT%H:%M:%SZ")
+    data["current_datetime"] = current_time
+    
+    return Response(data, status=200)
